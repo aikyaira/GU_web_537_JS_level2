173b076a8da6f8b52bc2ffbebfc7691f460b623a
Alina Monakova <aikyaira@gmail.com>
2020-10-28 13:00:28 +0300

    Tasks 1&2&3 complete

--- lesson 5/js/main.js
+++ lesson 5/js/main.js
@@ -6,6 +6,14 @@ const app = new Vue({
         catalogUrl: '/catalogData.json',
         products: [],
         imgCatalog: 'https://placehold.it/200x150',
+        searchLine: '',
+        filteredProducts: [],
+        isVisibleCart: false,
+        cartUrl: '/getBasket.json',
+        addToBasketURL: '/addToBasket.json',
+        deleteFromBasketUrl: '/deleteFromBasket.json',
+        cartProducts: [],
+
     },
     methods: {
         getJson(url) {
@@ -15,38 +23,105 @@ const app = new Vue({
                     console.log(error);
                 })
         },
-        addProduct(product) {
-            console.log(product.id_product)
+        addProduct(id) {
+            this.getJson(`${API}${this.addToBasketURL}`)
+                .then(data => {
+                    if (data.result === 1) {
+                        const existedItem = this.cartProducts.find(product => product.id_product === +id);
+                        if (existedItem) {
+                            existedItem.quantity += 1;
+                        } else {
+                            const nonExistedItem = this.products.find(product => product.id_product === +id);
+                            nonExistedItem.quantity = 1;
+                            this.cartProducts.push(nonExistedItem);
+                        }
+                    }
+                    else {
+                        alert('Error');
+                    }
+                });
+
+            //console.log(product.id_product)
+        },
+        deleteProduct(id) {
+            this.getJson(`${API}${this.deleteFromBasketUrl}`)
+                .then(data => {
+                    if (data.result === 1) {
+
+                        const existedItem = this.cartProducts.find(product => product.id_product === +id)
+                        if (existedItem && existedItem.quantity != 1) {
+                            existedItem.quantity -= 1;
+                        } else {
+                            this.cartProducts = this.cartProducts.filter((nextItem) => (nextItem.id_product != id));
+                        }
+                    }
+                    else {
+                        alert('Error');
+                    }
+                });
+
+        },
+        search() {
+            if (this.searchLine === "") {
+                this.filteredProducts = this.products;
+            } else {
+                let regexp = new RegExp(this.searchLine, 'i');
+                this.filteredProducts = this.products.filter(el => regexp.test(el.product_name));
+            }
+
+        },
+        cartVisibility() {
+            if (this.isVisibleCart) {
+                this.isVisibleCart = false
+            } else {
+                this.isVisibleCart = true
+            }
         },
     },
+    computed: {
+        isProductsInCart() {
+            if (this.cartProducts.length > 0) {
+                return true
+            } else {
+                return false
+            }
+        }
+    },
     beforeCreate() {
-        console.log('beforeCreate');
+        //console.log('beforeCreate');
     },
     created() {
-        console.log('created');
+        //console.log('created');
         this.getJson(`${API}${this.catalogUrl}`)
             .then(data => {
                 for (el of data) {
                     this.products.push(el);
                 }
+                this.filteredProducts = this.products;
+            });
+        this.getJson(`${API}${this.cartUrl}`)
+            .then(data => {
+                for (el of data.contents) {
+                    this.cartProducts.push(el);
+                }
             });
     },
     beforeMount() {
-        console.log('beforeMount');
+        //console.log('beforeMount');
     },
     mounted() {
-        console.log('mounted');
+        //console.log('mounted');
     },
     beforeUpdate() {
-        console.log('beforeUpdate');
+        //console.log('beforeUpdate');
     },
     updated() {
-        console.log('updated');
+        //console.log('updated');
     },
     beforeDestroy() {
-        console.log('beforeDestroy');
+        //console.log('beforeDestroy');
     },
     destroyed() {
-        console.log('destroyed');
+        //console.log('destroyed');
     },
 });
